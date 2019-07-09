10f054df5dc8747c38a71f87f8c668a2aba5d0bc
Roman Zvyazok <roman.zvyazok@eleks.com>
2019-07-09 20:06:23 +0300

    lily hepling

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.send(req.body);
+});
+router.post('/',function(req,res){
+    console.log(req.body);      // your JSON
 });
 
 module.exports = router;
