0c9567697e065c02b69b7a64a202fd4f829b7d70
roman1488 <roman.zvyazok@eleks.com>
2019-07-09 20:21:26 +0300

    change url for lily

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send(req.body);
+});
+router.post('/',function(req,res){
+    console.log(req.body);      // your JSON
+    res.setHeader('Content-Type', 'application/json');
+    res.end(req.body);
 });
 
 module.exports = router;
